98c251d1083daeb9d0e488a795a204e7a9fb13d4
daniele-bit <daniele.fas95@gmail.com>
2021-02-25 12:52:19 +0100

    modifica

--- rubrica-multipag-his/src/app/ricerca-contatti/ricerca-contatti.component.ts
+++ rubrica-multipag-his/src/app/ricerca-contatti/ricerca-contatti.component.ts
@@ -28,7 +28,7 @@ export class RicercaContattiComponent implements OnInit {
     this.listaCompletaVis = false;
     this.listaFiltrataVis = true;
     this.listaFiltrata = this.med.lista;
-    this.listaFiltrata = this.listaFiltrata.filter(p=>p.nome == this.criterioRicerca || p.cognome == this.criterioRicerca || p.telefono == this.criterioRicerca);
+    this.listaFiltrata = this.listaFiltrata.filter(p=>p.nome.startsWith(this.criterioRicerca)  || p.cognome.startsWith(this.criterioRicerca) || p.telefono.startsWith(this.criterioRicerca) );
 
   }
 }
