32a53cd90376c323fa00308fd949e3cab014e1dc
daniele-bit <daniele.fas95@gmail.com>
2021-02-25 12:38:00 +0100

    Aggiunta ricerca totale

--- rubrica-multipag-his/src/app/ricerca-contatti/ricerca-contatti.component.ts
+++ rubrica-multipag-his/src/app/ricerca-contatti/ricerca-contatti.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ModeratorService } from '../moderator.service';
+import { Persona } from '../persona';
 
 @Component({
   selector: 'app-ricerca-contatti',
@@ -8,12 +9,26 @@ import { ModeratorService } from '../moderator.service';
   styleUrls: ['./ricerca-contatti.component.css']
 })
 export class RicercaContattiComponent implements OnInit {
+
+  criterioRicerca: string;
+  listaFiltrata: Persona[]=[];
+  listaCompletaVis: boolean = true;
+  listaFiltrataVis: boolean = false;
   constructor(private router: Router, public med:ModeratorService) { }
+
 
   ngOnInit(): void {
   }
 
   mainPage(){
     this.router.navigateByUrl('/home-page');
+  }
+
+  cerca(){
+    this.listaCompletaVis = false;
+    this.listaFiltrataVis = true;
+    this.listaFiltrata = this.med.lista;
+    this.listaFiltrata = this.listaFiltrata.filter(p=>p.nome == this.criterioRicerca || p.cognome == this.criterioRicerca || p.telefono == this.criterioRicerca);
+
   }
 }
